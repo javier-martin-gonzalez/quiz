06515fec6eee5900f6db9dc7b142f252be9d0ffe
javier-martin-gonzalez <javi.martin.gonzalez@gmail.com>
2015-06-17 19:19:30 +0200

    Base de Datos SQLITE con 1 pregunta
    Base de Datos SQLITE con 1 pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,31 @@
+// SE INDICA QUE ESTAMOS TRABAJANDO CON EL MODELO (models.js)
+var models = require('../models/models.js');
 
 // GET /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	// models.Quiz.findAll()	o 	find()
+	//		---> se buscan datos en la tabla Quiz
+	//  Con findAll() --> se busca el array de elementos de la tabla Quiz
+	//	como solo tiene una pregunta en la tabla: se coge ... quiz[0]
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	})
+	//	PREGUNTA DIRECTA: res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 // GET /quizes/answer
-exports.answer = function(req, res){
-	if (req.query.respuesta === 'Roma'){
+exports.answer = function(req, res){	
+	models.Quiz.findAll().success(function(quiz){
+		if (req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		}else{
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	})
+	
+	/*if (req.query.respuesta === 'Roma'){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	}else{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	}*/
 };
---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+﻿// Definicion del modelo de Quiz
+//	+ Se define la estructura de la tabla de quizes (preguntas)
+//		---> 2 campos PREGUNTA y RESPUESTA (los dos de tipo string)
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define('Quiz',
+					{ pregunta: DataTypes.STRING,
+					  respuesta: DataTypes.STRING
+					});
+}
