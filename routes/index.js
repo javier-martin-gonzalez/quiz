970fa4426165f19857f0056adf63f2619a096710
javier-martin-gonzalez <javi.martin.gonzalez@gmail.com>
2015-06-17 07:55:18 +0200

    Primera Pregunta
    Añadida la primera pregunta con su respuesta. Cambiado también el index

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,25 @@
 var express = require('express');
 var router = express.Router();
 
+// YO: SE INCLUYE EL CONTROLADOR
+// =====================================
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
 	// YO: con res.render --> se invoca a la VIEW (views/index.ejs)
 	// 	res.render --> se generan y envian al cliente la Vista EJS
 	//		y se le pasa como parámetro "title" con el valor Express
 	// =============================================================
+	// YO: esta ruta genera directamente la VIEW sin pasar por el CONTROLLER
   res.render('index', { title: 'Quiz' });
 });
+
+// YO: RUTA /quizes/question	--> se llama al Controller: quizController.question
+router.get('/quizes/question', quizController.question);
+
+// YO: RUTA /quizes/answer	--> se llama al Controller: quiz_controller.answer
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
